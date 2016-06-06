8f24326d28fb4d1f3e2b90beebc5027d214f667c
baptistePrunot <ipodbat@gmail.com>
2016-06-06 17:16:28 +0200

    Le bouclier s'affiche, la collision reste a faire

--- src/space/Bouclier.java
+++ src/space/Bouclier.java
@@ -5,10 +5,38 @@
  */
 package space;
 
+import iut.Game;
+import iut.Objet;
+
 /**
  *
  * @author bapt
  */
-public class Bouclier {
+public class Bouclier extends iut.ObjetTouchable{
+
+    public Bouclier(Game g, int x, int y) {
+        super(g, "shield", x, y);
+    }
+
+    @Override
+    public void effect(Objet o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isFriend() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEnnemy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void move(long dt) {
+        this.moveX(-50);
+    }
+    
     
 }
